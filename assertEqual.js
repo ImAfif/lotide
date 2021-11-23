f1efc7e2bc790782536759d095536759b86828bc
ImAfif <afifadelayoub@gmail.com>
2021-11-23 06:27:09 +0000

    commit msg

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,9 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return " ✅✅✅ Assertion Passed: " + actual + " === " + expected;
-  } else {
-    return " 🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected;
+  if (actual === expected){
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`
+  } else { 
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
   }
-};
-console.log(assertEqual("afdadfa", "asdfsf"));
+}
+console.log(assertEqual("1", "2"))
+
---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+function head(array) {
+  console.log(array[0]);
+}
+
+
+assertEqual(1, 2);
+
+console.log(assertEqual("1", "2"));
+
+
+console.log(assertEqual(head([5,6,7]), 5));
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
